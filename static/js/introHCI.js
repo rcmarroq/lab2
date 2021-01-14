bdbcec8d62999319b04d051dfc7d59a7fdefe5ab
Ruby Celeste Marroquin <rcmarroq@ucsd.edu>
2021-01-14 01:32:58 -0500

    Updated inddex file according to slides, linked JS to button now functional in test

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-    initializePage();
-});
+$(document).ready(function () {
+  initializePage()
+})
 
-function initializePage() {
-    $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+function initializePage () {
+  $('#testjs').click(function (e) {
+    $('.jumbotron h1').text('Javascript is connected')
+  })
+
+  $('a.thumbnail').click(projectClick)
+}
+
+function projectClick (e) {
+  e.preventDefault()
+  $(this).css('background-color', '#7fff00')
 }
